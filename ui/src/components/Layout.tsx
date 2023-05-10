3cc54aae9b8f5949e273bd81e719b9bf68ebcc60
Huy Nguyen <giahuy2201@icloud.com>
2023-05-10 09:47:53 -0400

    Add navbar

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -1,20 +1,22 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 import { Outlet } from "react-router-dom";
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import NavBar from "./NavBar";
+import { CssBaseline } from "@mui/material";
 
 type LayoutProps = {
-    children: ReactNode
-}
+    children: ReactNode;
+};
 
 export default function Layout(props: LayoutProps) {
     return (
-        <Container>
-            <Box>
-                {props.children}
-            </Box>
-        </Container>
-    )
+        <div>
+            <CssBaseline />
+            <NavBar />
+            <Container>{props.children}</Container>
+        </div>
+    );
 }
