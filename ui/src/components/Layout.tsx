258773dedb49c8d88a9e9f42e843308a92be092b
Huy Nguyen <giahuy2201@icloud.com>
2023-05-20 12:25:00 -0400

    Add system-based dark mode

--- ui/src/components/Layout.tsx
+++ ui/src/components/Layout.tsx
@@ -5,21 +5,32 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import NavBar from "./NavBar";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-
-const darkTheme = createTheme({
-    palette: {
-        mode: "dark",
-    },
-});
+import {
+    CssBaseline,
+    ThemeProvider,
+    createTheme,
+    useMediaQuery,
+} from "@mui/material";
 
 type LayoutProps = {
     children: ReactNode;
 };
 
 export default function Layout(props: LayoutProps) {
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? "dark" : "light",
+                },
+            }),
+        [prefersDarkMode]
+    );
+
     return (
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
             <NavBar />
             {props.children}
