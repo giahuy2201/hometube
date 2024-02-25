3ebe283bbc4b87fee170605dc327361e7175c082
Huy Nguyen <giahuy2201@icloud.com>
2024-02-25 11:05:08 -0500

    Implement polymorphism directly in Task schemas

--- src/daemon/schemas.py
+++ src/daemon/schemas.py
@@ -2,9 +2,15 @@ from typing import List
 from enum import Enum
 from datetime import datetime
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+import datetime
 
-from medias.schemas import Media
 from presets.schemas import Preset
+from ytdlp.downloader import YTdlp
+import medias.schemas as medias_schemas
+import medias.crud as medias_crud
+import presets.utils as presets_utils
+import crud as tasks_crud
 
 
 class TaskType(str, Enum):
@@ -22,6 +28,7 @@ class TaskStatus(str, Enum):
     Running = "running"
     Finished = "finished"
     Scheduled = "scheduled"
+    Failed = "failed"
 
 
 class TaskCreate(BaseModel):
@@ -36,8 +43,39 @@ class TaskCreate(BaseModel):
 class Task(TaskCreate):
     id: int
 
-    media: Media
+    media: medias_schemas.Media
     preset: Preset
 
     class Config:
         from_attributes = True
+
+    def __mark_finished(self, db: Session):
+        self.when = datetime.datetime.now()
+        self.status = "finished"
+        tasks_crud.update_task(db, self)
+
+    def run(self, db: Session):
+        pass
+
+
+class DownloadTask(Task):
+    def run(self, db: Session):
+        ytdlp = YTdlp(self.media.webpage_url)
+        print(f"Download {self.media.webpage_url}")
+        ytdlp.get_content(self.preset)
+        self.__mark_finished(db)
+
+
+class ImportTask(Task):
+    def run(self, db: Session):
+        file_location = presets_utils.__infer_path(self.preset, self.media)
+        version_id = f"{self.media_id}-{self.preset_id}"
+        print(f"Import {file_location}")
+        version = medias_schemas.MediaVersion(
+            id=version_id,
+            location=file_location,
+            preset_id=self.preset_id,
+            media_id=self.media_id,
+        )
+        medias_crud.create_version(db, version)
+        self.__mark_finished(db)
---
+++ src/presets/utils.py
@@ -0,0 +1,12 @@
+import medias.schemas as medias_schemas
+import presets.schemas as presets_schemas
+
+
+def __infer_path(preset: presets_schemas.Preset, media: medias_schemas.Media):
+    file_ext = "m4a" if preset.squareCover else "mkv"
+    file_name = preset.template % {
+        "title": media.title,
+        "id": media.id,
+        "ext": file_ext,
+    }
+    return f"{preset.destination}/{file_name}"
