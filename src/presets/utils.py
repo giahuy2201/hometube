aaee26b2db2853800c7475a33502735318348d04
Huy Nguyen <giahuy2201@icloud.com>
2024-02-28 01:31:02 -0500

    Fix unsanitized filename in .location of media version

--- src/presets/utils.py
+++ src/presets/utils.py
@@ -1,5 +1,6 @@
 import medias.schemas as medias_schemas
 import presets.schemas as presets_schemas
+from yt_dlp.utils import sanitize_filename
 
 
 def get_media_path(preset: presets_schemas.Preset, media: medias_schemas.Media):
@@ -8,8 +9,12 @@ def get_media_path(preset: presets_schemas.Preset, media: medias_schemas.Media):
 
 def infer_file_name(preset: presets_schemas.Preset, media: medias_schemas.Media):
     file_ext = "flac" if preset.squareCover else "mkv"
-    return preset.template % {
-        "title": media.title,
-        "id": media.id,
-        "ext": file_ext,
-    }
+    # Title in info.json and media file name could be different since yt-dlp replace chars like *:<>?|/\\ with their respective full width versions to avoid filename problems on different fs
+    return sanitize_filename(  # mimic the way yt-dlp sanitize file names
+        preset.template
+        % {
+            "title": media.title,
+            "id": media.id,
+            "ext": file_ext,
+        }
+    )
