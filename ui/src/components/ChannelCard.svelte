2f23e969c88c2b60e1e5a4a8e9c50e80231810b0
Huy Nguyen <giahuy2201@icloud.com>
2024-03-21 15:13:05 -0400

    Enable clickable ChannelCard

--- ui/src/components/ChannelCard.svelte
+++ ui/src/components/ChannelCard.svelte
@@ -4,14 +4,16 @@
   export let channel: Channel;
 </script>
 
-<div class="card channel-card">
-  <div class="card-top">
-    <div class="card-image">
-      <img src={channel.thumbnail} alt={channel.uploader_id} />
+{#if channel}
+  <a class="card channel-card" href="/channels/{channel.channel_id}">
+    <div class="card-top">
+      <div class="card-image">
+        <img src={channel.thumbnail} alt={channel.uploader_id} />
+      </div>
     </div>
-  </div>
-  <div class="card-bottom">
-    <div class="card-title">{channel.channel}</div>
-    <div class="card-subtitle">{channel.channel_follower_count} subs</div>
-  </div>
-</div>
+    <div class="card-bottom">
+      <div class="card-title">{channel.channel}</div>
+      <div class="card-subtitle">{channel.channel_follower_count} subs</div>
+    </div>
+  </a>
+{/if}
