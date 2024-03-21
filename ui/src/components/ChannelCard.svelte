6d062f0a2946fc50c2910240e4a4d73008b483c2
Huy Nguyen <giahuy2201@icloud.com>
2024-03-21 14:04:21 -0400

    Add ui ChannelCard

---
+++ ui/src/components/ChannelCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Channel } from "../interfaces/Channel.interface";
+
+  export let channel: Channel;
+</script>
+
+<div class="card channel-card">
+  <div class="card-top">
+    <div class="card-image">
+      <img src={channel.thumbnail} alt={channel.uploader_id} />
+    </div>
+  </div>
+  <div class="card-bottom">
+    <div class="card-title">{channel.channel}</div>
+    <div class="card-subtitle">{channel.channel_follower_count}</div>
+  </div>
+</div>
