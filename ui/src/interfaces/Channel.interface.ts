90e8cf4f92a77207f9d42fb55503d9f892dd785a
Huy Nguyen <giahuy2201@icloud.com>
2024-03-21 13:33:07 -0400

    Add ui Channel interface

---
+++ ui/src/interfaces/Channel.interface.ts
@@ -0,0 +1,12 @@
+
+export interface Channel {
+    channel: string;
+    channel_id: string;
+    channel_url: string;
+    uploader: string;
+    uploader_id: string;
+    uploader_url: string;
+    thumbnail: string;
+    description: string;
+    channel_follower_count: number;
+}
