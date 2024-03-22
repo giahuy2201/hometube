d2963ae12be6f580de2fc42445e2b67a379a0e26
Huy Nguyen <giahuy2201@icloud.com>
2024-03-22 10:59:47 -0400

    Make MediaCard clickable

--- ui/src/components/MediaCard.svelte
+++ ui/src/components/MediaCard.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-  import "../app.css";
   import type { Media } from "../interfaces/Media.interface";
 
   export let media: Media;
 </script>
 
-<div class="card media-card">
-  <div class="card-top">
-    <div class="card-image">
-      <img src={media.thumbnail} alt={media.uploader_id} />
+{#if media}
+  <a class="card media-card" href="/medias/{media.id}">
+    <div class="card-top">
+      <div class="card-image">
+        <img src={media.thumbnail} alt={media.uploader_id} />
+      </div>
+      <div class="card-overlay"><div>{media.duration}s</div></div>
     </div>
-    <div class="card-overlay"><div>{media.duration}</div></div>
-  </div>
-  <div class="card-bottom">
-    <div class="card-title">{media.title}</div>
-    <div class="card-subtitle">{media.uploader} - {media.upload_date}</div>
-  </div>
-</div>
+    <div class="card-bottom">
+      <div class="card-title">{media.title}</div>
+      <div class="card-subtitle">{media.uploader} - {media.upload_date}</div>
+    </div>
+  </a>
+{/if}
