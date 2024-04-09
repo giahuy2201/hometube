f2b0c3b1797f85dcacc165ce444e568bc0c7cf1f
Huy Nguyen <giahuy2201@icloud.com>
2024-04-09 13:11:47 -0400

    Fix ytdlp query_medias return format

--- src/ytdlp/router.py
+++ src/ytdlp/router.py
@@ -10,19 +10,21 @@ router = APIRouter()
 
 @router.get("/", response_model=list[medias_schemas.Media])
 def query_medias(term: str = Query(None)):
-    media_id = None
+    medias: list[medias_schemas.Media] = []
     if term.startswith("id:"):
+        media_id = None
         media_id = term.split("id:")[1].strip()
+        ytdlp = YTdlp(f"https://youtu.be/{media_id}")
+        newMedia = ytdlp.get_media()
+        if not newMedia:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Failed to add media with media_id: {media_id}",
+            )
+        medias.append(newMedia)
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Searching with non-id term is currently not supported",
         )
-    ytdlp = YTdlp(f"https://youtu.be/{media_id}")
-    newMedia = ytdlp.get_media()
-    if not newMedia:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Failed to add media with media_id: {media_id}",
-        )
-    return newMedia
+    return medias
