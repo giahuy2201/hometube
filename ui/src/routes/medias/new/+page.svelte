e263487c75f8166f3126afe27bab91fd9952ba7c
Huy Nguyen <giahuy2201@icloud.com>
2024-04-09 12:29:02 -0400

    Add Media new page ui

---
+++ ui/src/routes/medias/new/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let placeholder = "Search...";
+    export let value = "";
+    export let onInput = () => {};
+</script>
+
+<div class="relative text-gray-600 m-4">
+    <input
+        type="search"
+        class="w-full py-2 pl-4 pr-4 rounded-lg border border-gray-300 focus:outline-none focus:border-indigo-500"
+        {placeholder}
+        {value}
+        on:input={onInput}
+    />
+</div>
+
+<style>
+</style>
