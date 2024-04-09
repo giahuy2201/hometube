8c5ad87c90b0ff81ae2c7b50d3258ce21701e14c
Huy Nguyen <giahuy2201@icloud.com>
2024-04-09 13:39:57 -0400

    Add search media with id ui

--- ui/src/routes/medias/new/+page.svelte
+++ ui/src/routes/medias/new/+page.svelte
@@ -1,18 +1,49 @@
 <script lang="ts">
-    export let placeholder = "Search...";
-    export let value = "";
-    export let onInput = () => {};
+    import { writable } from "svelte/store";
+    import type { Media } from "../../../interfaces/Media.interface";
+    import MediaCard from "../../../components/MediaCard.svelte";
+
+    export let term = "";
+
+    const foundMedias = writable<Media[]>();
+
+    const searchMedia = async (term: string) => {
+        try {
+            const response = await fetch(
+                "http://localhost:8000/api/ytdlp/?term=" + term,
+            );
+            const data = await response.json();
+            foundMedias.set(data);
+        } catch (error) {
+            console.error("Error search medias:", error);
+        }
+    };
+    function handleKeyPress(event: { key: string }) {
+        if (event.key === "Enter") {
+            searchMedia(term);
+        }
+    }
 </script>
 
 <div class="relative text-gray-600 m-4">
     <input
         type="search"
         class="w-full py-2 pl-4 pr-4 rounded-lg border border-gray-300 focus:outline-none focus:border-indigo-500"
-        {placeholder}
-        {value}
-        on:input={onInput}
+        placeholder="eg. id:####"
+        bind:value={term}
+        on:keydown={handleKeyPress}
     />
 </div>
+
+{#if $foundMedias}
+    <ul class="">
+        {#each $foundMedias as media}
+            <li>
+                <MediaCard {media} fullWidth />
+            </li>
+        {/each}
+    </ul>
+{/if}
 
 <style>
 </style>
